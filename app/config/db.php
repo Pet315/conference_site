62b797fed59462780ee087bc86cb51bee3c6f464
Pet315 <oppapeter17@gmail.com>
2022-08-08 22:57:41 +0300

    Add changes

--- app/config/db.php
+++ app/config/db.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'name' => 'conference_site',
-    'user' => 'root',
+    'host' => '',
+    'name' => '',
+    'user' => '',
     'password' => '',
 ];
